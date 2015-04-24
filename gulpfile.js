0e7030e9d250b4d54c012051003fcc395b502352
Ariel Mac <ariel@pchara.com>
2015-04-24 01:56:42 -0300

    v2015

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    gls = require('gulp-live-server'),
+    nunjucks = require('gulp-nunjucks-html');
 
-gulp.task('scss',function(){
-    return gulp.src('html/css/*.scss')
-        .pipe(plumber())
-        .pipe(sass({errLogToConsole: true}))
-        .pipe(gulp.dest('html/css'));
-});
+    gulp.task('serve', function() {
+        var server = gls.static(['./html/','./ static/'],8080);
+            server.start();
+            gulp.watch(['static/**/*.css','static/**/*.html','static/**/*.js'], server.notify);
+    });
 
-gulp.task('watch', function() {
-    gulp.watch('html/css/*.scss', ['scss']);
-});
+    gulp.task('html', function() {
+        gulp.watch('src/templates/**/*.html', ['html']);
+        return gulp.src('src/templates/*.html')
+                .pipe(nunjucks({
+                  searchPaths: ['src/templates']
+                }))
+                .pipe(gulp.dest('static'));
+    });
 
-gulp.task('default',['scss','watch']);
+    gulp.task('scss',function(){
+        gulp.watch('html/css/*.scss', ['scss']);
+        return gulp.src('html/css/*.scss')
+                .pipe(plumber())
+                .pipe(sass({errLogToConsole: true}))
+                .pipe(gulp.dest('html/css'));
+    });
+
+    gulp.task('default',['scss','html','serve',]);
--- html/js/controllers.js
+++ html/js/controllers.js
@@ -50,7 +50,6 @@ portifa.controller('insta',['$scope','$http',function($scope, $http){
 	$scope.loadig = true;
 	var link = 'https://api.instagram.com/v1/users/31132697/media/recent/?client_id=831e367d117c42d4ac88f8fd14aef654&callback=JSON_CALLBACK';
 	$http.jsonp(link).success(function(response){
-		console.log(response);
 		$scope.list = response.data;
 		$scope.loadig = false;
 	});
