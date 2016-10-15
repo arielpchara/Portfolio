84d2b9c8f1ec93782b2510fef661ec1f701c1f20
Ariel Pchara <ariel@pchara.com>
2016-10-15 00:35:37 -0300

    2015 version

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,61 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    // plumber = require('gulp-plumber'),
-    gls = require('gulp-live-server');
-    // nunjucks = require('gulp-nunjucks-html');
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    plumber = require('gulp-plumber'),
+    nunjucks = require('gulp-nunjucks-html'),
+    minifyHTML = require('gulp-minify-html'),
+    gls = require('gulp-live-server'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require("gulp-rename");
 
-    gulp.task('serve', function() {
-        var server = gls.static(['./html/','./ static/'],8080);
-            server.start();
-            gulp.watch(['static/**/*.css','static/**/*.html','static/**/*.js'], server.notify);
-    });
+gulp.task('serve', function() {
+    var server = gls.static('./', 3000);
+    server.start();
+    gulp.watch(['css/*.css','js/*.js','*.html'], server.notify);
+});
 
-    gulp.task('html', function() {
-        gulp.watch('src/templates/**/*.html', ['html']);
-        return gulp.src('src/templates/*.html')
-                .pipe(nunjucks({
-                  searchPaths: ['src/templates']
-                }))
-                .pipe(gulp.dest('static'));
-    });
 
-    gulp.task('scss',function(){
-        gulp.watch('html/css/*.scss', ['scss']);
-        return gulp.src('html/css/*.scss')
-                // .pipe(plumber())
-                .pipe(sass({errLogToConsole: true}))
-                .pipe(gulp.dest('html/css'));
-    });
+gulp.task('scss',function(){
+    return gulp.src(['css/src/*.scss'])
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
 
-    gulp.task('default',['scss','serve']);
+gulp.task('concat',function(){
+    return gulp.src(['js/src/*.js'])
+        .pipe(plumber())
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('js'));
+});
+
+gulp.task('uglify',['concat'],function(){
+    return gulp.src(['js/main.js'])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(gulp.dest('js'));
+});
+
+gulp.task('html',function(){
+    return gulp.src('src/*.html')
+    .pipe(plumber())
+    .pipe(nunjucks({
+        searchPaths: ['src']
+    }))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch',function(){
+    gulp.watch(['css/src/**/*.scss'],['scss']);
+    gulp.watch(['src/**/*.html'],['html']);
+    gulp.watch(['js/src/**/*.js'],['uglify']);
+});
+
+gulp.task('default',['watch','scss','uglify','html','serve']);
--- html/bower_components/jquery-scrollsnap/README.md
+++
@@ -1,6 +0,0 @@
-jquery-scrollsnap-plugin
-========================
-
-Javascript implementation of vertical scroll snapping.
-
-Demo and documentation at http://benoit.pointet.info/stuff/jquery-scrollsnap-plugin/ .
